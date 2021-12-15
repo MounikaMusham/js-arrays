802a6c044dc0970ab8ab2a5ebcde15ebdbd81862
mounika <mounikamusham12@gmail.com>
2021-12-15 18:11:40 +0530

    array

---
+++ array.js
@@ -0,0 +1,27 @@
+var array1 =["apple","mango","grapes","orange",1,2,4,7];
+//var array2 = new Array("one","two")
+
+console.log(array1[3]);
+
+ array1.push("babana"); //push
+console.log(array1);
+
+ array1.unshift("one");//unshift
+ console.log(array1);
+
+array1[2]="Hello";  //inserting new element
+console.log(array1);
+
+array1.pop();//pop
+ console.log(array1);
+
+console.log(array1.length);//length
+
+array1.shift();  //shift
+ console.log(array1);
+
+array1.sort();    //sort
+console.log(array1);
+
+array1.reverse();    //reverse
+console.log(array1);
